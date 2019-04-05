b38a2fefcec1e15a0e2aa0b055cc74dc262a6819
Marlon Rocha <marlon.rocha@madeiramadeira.local>
2019-04-05 10:33:29 -0300

    Projeto adicionado ao repositório.

---
+++ app/src/main/java/com/example/jamiltondamasceno/listadetarefas/model/Tarefa.java
@@ -0,0 +1,29 @@
+package com.example.jamiltondamasceno.listadetarefas.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by jamiltondamasceno
+ */
+
+public class Tarefa implements Serializable {
+
+    private Long id;
+    private String nomeTarefa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeTarefa() {
+        return nomeTarefa;
+    }
+
+    public void setNomeTarefa(String nomeTarefa) {
+        this.nomeTarefa = nomeTarefa;
+    }
+}
